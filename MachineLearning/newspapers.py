88e81a3df032b84f63315e754eaaf67aed9964a4
blacklungLL <145188803+blacklungLL@users.noreply.github.com>
2025-06-13 14:35:42 +0300

    Update newspapers.py

--- MachineLearning/newspapers.py
+++ MachineLearning/newspapers.py
@@ -22,11 +22,11 @@ nltk.download('wordnet')
 
 # def load_data_from_folders(base_path):
 #     data = []
-#     categories = os.listdir(base_path)  # Получаем список папок (категорий)
+#     categories = os.listdir(base_path)
 #
 #     for category in categories:
 #         category_path = os.path.join(base_path, category)
-#         if os.path.isdir(category_path):  # Проверяем, что это папка
+#         if os.path.isdir(category_path):
 #             for filename in os.listdir(category_path):
 #                 file_path = os.path.join(category_path, filename)
 #                 with open(file_path, 'r', encoding='utf-8', errors='ignore') as file:
@@ -54,22 +54,17 @@ lemmatizer = WordNetLemmatizer()
 
 def preprocess_text(text):
     # Токенизация
-    tokens = word_tokenize(text.lower(), language='english')  # Явно указываем язык
-    # Удаление стоп-слов и пунктуации
+    tokens = word_tokenize(text.lower(), language='english')
     tokens = [word for word in tokens if word.isalnum() and word not in stop_words]
-    # Лемматизация
     tokens = [lemmatizer.lemmatize(word) for word in tokens]
     return " ".join(tokens)
 
 
-# Применение функции предобработки
 data['cleaned_text'] = data['text'].apply(preprocess_text)
 
-# Кодирование меток
 label_encoder = LabelEncoder()
 data['category_encoded'] = label_encoder.fit_transform(data['category'])
 
-# Разделение данных
 X_train, X_test, y_train, y_test = train_test_split(
     data['cleaned_text'],
     data['category_encoded'],
@@ -78,20 +73,16 @@ X_train, X_test, y_train, y_test = train_test_split(
 )
 
 
-# TF-IDF Vectorizer
-vectorizer = TfidfVectorizer(max_features=5000)  # Ограничим количество признаков до 5000
+vectorizer = TfidfVectorizer(max_features=5000)
 X_train_tfidf = vectorizer.fit_transform(X_train)
 X_test_tfidf = vectorizer.transform(X_test)
 
 
-# Обучение логистической регрессии
 lr_model = LogisticRegression(max_iter=1000)
 lr_model.fit(X_train_tfidf, y_train)
 
-# Предсказания
 y_pred_lr = lr_model.predict(X_test_tfidf)
 
-# Оценка модели
 print("Logistic Regression Accuracy:", accuracy_score(y_test, y_pred_lr))
 print(classification_report(y_test, y_pred_lr, target_names=label_encoder.classes_))
 
@@ -99,14 +90,11 @@ print(classification_report(y_test, y_pred_lr, target_names=label_encoder.classe
 rf_model = RandomForestClassifier(n_estimators=100, random_state=42)
 rf_model.fit(X_train_tfidf, y_train)
 
-# Предсказания
 y_pred_rf = rf_model.predict(X_test_tfidf)
 
-# Оценка модели
 print("Random Forest Accuracy:", accuracy_score(y_test, y_pred_rf))
 print(classification_report(y_test, y_pred_rf, target_names=label_encoder.classes_))
 
-# Визуализация распределения категорий
 plt.figure(figsize=(8, 6))
 sns.countplot(x='category', data=data)
 plt.title("Распределение категорий")
